ebb082c7d78af902bab831514e985d6c32e9cca6
sudipmeta <115703075+sudipmeta@users.noreply.github.com>
2022-11-25 15:35:39 +0530

    Add files via upload

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,110 +1,112 @@
-function showNext(element) {
-    var c = element.parentNode;
-    c.style.display = "none";
-    // c.style.border = "0px";
-    var ns = next(c);
-    // if (ns.id === "genderItem") {
-        ns.style.display = "block";
-        ns.style.border= "none";
+let showNext = (element) => {
+    let c = element.parentNode;
+    c.style.display = `none`;
+    // c.style.border = `0px`;
+    let ns = next(c);
+    // if (ns.id === `genderItem`) {
+        ns.style.display = `block`;
+        ns.style.border= `none`;
     // }
     // else {
-        // ns.style.display = "flex";
+        // ns.style.display = `flex`;
     // }
 }
-function next(elem) {
+let next = (elem) => {
     do {
         elem = elem.nextSibling;
     } while (elem && elem.nodeType !== 1);
     return elem;
 }
 
-function setName(element){
-    let empName = document.getElementById("fname").value;
+let setName = (element) => {
+    let empName = document.getElementById(`fname`).value;
     
     if(/\d/.test(empName) || empName.length <=2){
-        alert("Invalid Name");
+        alert(`Invalid Name`);
     }
     else{
-        document.getElementById("genderInput").innerHTML = "Hi, " + empName + "! Can i know your gender";
+        document.getElementById(`genderInput`).innerHTML = `Hi, ${empName}! Can i know your gender`;
         showNext(element);
     }
-}function sectionNext(element) {
-    var c = element.parentNode;
-    var x = c.parentNode;
-    var y = x.parentNode;
-    var z = y.parentNode;
-    z.style.display = "none";
-    var ns = next(z);
-    // if (ns.id === "genderItem") {
-        ns.style.display = "block";
+}
+
+let sectionNext = (element) => {
+    let c = element.parentNode;
+    let x = c.parentNode;
+    let y = x.parentNode;
+    let z = y.parentNode;
+    z.style.display = `none`;
+    let ns = next(z);
+    // if (ns.id === `genderItem`) {
+        ns.style.display = `block`;
     // }
     // else {
-        // ns.style.display = "flex";
+        // ns.style.display = `flex`;
     // }
 }
 
-function borderChange(element){
+let borderChange = (element) => {
     if(element.value.length < 8){
-        element.style.border = "3px solid red";
+        element.style.border = `3px solid red`;
     }
     else if(element.value.length >= 8 && element.value.length <= 10){
-        element.style.border = "3px solid orange";
+        element.style.border = `3px solid orange`;
     }
     else{
-        element .style.border = "3px solid green";
+        element .style.border = `3px solid green`;
     }
 }
 
-function showPass(element){
-    let empMail = document.getElementById("email").value;
+let showPass = (element) =>{
+    let empMail = document.getElementById(`email`).value;
     
-    if(empMail.includes("@") && empMail.length >=3){
+    if(empMail.includes(`@`) && empMail.length >=3){
         showNext(element);
     }
     else{
-        alert("Invalid E-mail ID");
+        alert(`Invalid E-mail ID`);
     }
 }
 
-function showContact(element){
-    const pattern = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$");
-    let empPass = document.getElementById("password").value;
-    let empCPass = document.getElementById("cpassword").value;
+let showContact = (element) => {
+    const pattern = new RegExp(`^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$`);
+    let empPass = document.getElementById(`password`).value;
+    let empCPass = document.getElementById(`cpassword`).value;
 
     if(pattern.test(empPass) && empPass===empCPass){
         showNext(element);
     }
     else if(!pattern.test(empPass)){
-        alert("Password should contain atleast: \n one Uppercase character,\n atleast one  Lowercase character,\n atleast one Numeric value,\n atleast one Alphanumeric character \n\nAnd minimum length should be 8");
+        alert(`Password should contain atleast: \n one Uppercase character,\n atleast one  Lowercase character,\n atleast one Numeric value,\n atleast one Alphanumeric character \n\nAnd minimum length should be 8`);
     }
     else if(empPass!=empCPass){
-        alert("Passwords don't match");
+        alert(`Passwords don't match`);
     }
 }
 
 
 // function showVehicle(element){
-//     let contact = document.getElementById("number").value;
+//     let contact = document.getElementById(`number`).value;
 
 //     if(/^\d+$/.test(contact) && contact.length >= 8)
 //         showNextElement(element);
 //     else
-//         alert("Invalid Number");
+//         alert(`Invalid Number`);
 // }
 
-function setVehicle(element){
+let setVehicle = (element) => {
     selectElement = document.querySelector('#veh-type');
     output = selectElement.value;
     console.log(output);
     switch (output){
-        case("cycle"):
-            document.getElementById("cycle-price").style.display = "block";
+        case(`cycle`):
+            document.getElementById(`cycle-price`).style.display = `block`;
             break;
-        case("motor-cycle"):
-            document.getElementById("motor-cycle-price").style.display = "block";
+        case(`motor-cycle`):
+            document.getElementById(`motor-cycle-price`).style.display = `block`;
             break;
-        case("car"):
-            document.getElementById("car-price").style.display = "block";
+        case(`car`):
+            document.getElementById(`car-price`).style.display = `block`;
             break;
     }
     showNext(element);
@@ -112,37 +114,37 @@ function setVehicle(element){
 
 var price;
 
-function setPrice(element){
+let setPrice = (element) => {
     price = element.value;
-    document.getElementById("pricing-div").style.display = "none";
-    document.getElementById("pass").style.display = "block";
-    document.getElementById("pass-price").innerHTML = "Amount: Rs " + price;
+    document.getElementById(`pricing-div`).style.display = `none`;
+    document.getElementById(`pass`).style.display = `block`;
+    document.getElementById(`pass-price`).innerHTML = `Amount: Rs ${price}`;
 }
 
-function currencyChange(element){
-    if(element.id === "inr-btn"){
-        document.getElementById("pass-price").innerHTML = "Amount: Rs " + price;
-        document.getElementById("usd-btn").style.display = "block";
-        document.getElementById("yen-btn").style.display = "block";
-        document.getElementById("inr-btn").style.display = "none";
+let currencyChange = (element) => {
+    if(element.id === `inr-btn`){
+        document.getElementById(`pass-price`).innerHTML = `Amount: Rs ${price}`;
+        document.getElementById(`usd-btn`).style.display = `block`;
+        document.getElementById(`yen-btn`).style.display = `block`;
+        document.getElementById(`inr-btn`).style.display = `none`;
     }
-    else if(element.id === "usd-btn"){
-        document.getElementById("pass-price").innerHTML = "Amount: " + (price/81.79).toFixed(2) + "$";
-        document.getElementById("usd-btn").style.display = "none";
-        document.getElementById("yen-btn").style.display = "block";
-        document.getElementById("inr-btn").style.display = "block";
+    else if(element.id === `usd-btn`){
+        document.getElementById(`pass-price`).innerHTML = `Amount: ${(price/81.79).toFixed(2)}$`;
+        document.getElementById(`usd-btn`).style.display = `none`;
+        document.getElementById(`yen-btn`).style.display = `block`;
+        document.getElementById(`inr-btn`).style.display = `block`;
     }
-    else if(element.id === "yen-btn"){
-        document.getElementById("pass-price").innerHTML = "Amount: " + (price/0.58).toFixed(2) + " Yen";
-        document.getElementById("usd-btn").style.display = "block";
-        document.getElementById("yen-btn").style.display = "none";
-        document.getElementById("inr-btn").style.display = "block";
+    else if(element.id === `yen-btn`){
+        document.getElementById(`pass-price`).innerHTML = `Amount: ${(price/0.58).toFixed(2)} Yen`;
+        document.getElementById(`usd-btn`).style.display = `block`;
+        document.getElementById(`yen-btn`).style.display = `none`;
+        document.getElementById(`inr-btn`).style.display = `block`;
     }
 }
 
-function complete(){
-    document.getElementById("pass").style.display = "none";
-    document.getElementById("final").style.display = "block";
+let complete = () => {
+    document.getElementById(`pass`).style.display = `none`;
+    document.getElementById(`final`).style.display = `block`;
 }
 
 
